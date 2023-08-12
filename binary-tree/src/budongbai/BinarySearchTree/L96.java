3fc68eb9b0936de09bf136e8d636b78102d7fa13
budongbai <liulijuan132@qq.com>
2023-08-12 15:54:55 +0800

    L96 不同的二叉搜索树，补充参考注释

--- binary-tree/src/budongbai/BinarySearchTree/L96.java
+++ binary-tree/src/budongbai/BinarySearchTree/L96.java
@@ -1,15 +1,26 @@
 package budongbai.BinarySearchTree;
 
 /**
+ * <a href="https://leetcode.cn/problems/unique-binary-search-trees/description/">96. 不同的二叉搜索树</a>
  * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
  */
 public class L96 {
     /**
-     * [1, i-1] i [i+1, n]
-     * dp[i] = dp[i-1] + dp[
+     * 假设 n 个节点存在二叉排序树的个数是 G (n)，令 f(i) 为以 i 为根的二叉搜索树的个数，则
+     * G(n)=f(1)+f(2)+f(3)+f(4)+...+f(n)
+     * <p>
+     * 当 i 为根节点时，其左子树节点个数为 i-1 个，右子树节点为 n-i，则
+     * f(i)=G(i−1)∗G(n−i)
+     * <p>
+     * 综合两个公式可以得到 卡特兰数 公式
+     * G(n)=G(0)∗G(n−1)+G(1)∗(n−2)+...+G(n−1)∗G(0)
+     * <p>
+     * 作者：画手大鹏
+     * 链接：https://leetcode.cn/problems/unique-binary-search-trees/solutions/6693/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     public int numTrees(int n) {
-
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
